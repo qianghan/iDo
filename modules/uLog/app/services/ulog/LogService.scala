255254d111e7ce344d1785ee1a7b3770d0f5aa46
qiang.han <qiang.han@machool.com>
2015-02-20 11:57:00 -0800

    merging all eson changes

---
+++ modules/uLog/app/services/ulog/LogService.scala
@@ -0,0 +1,12 @@
+RawBlameHistory   
+package services.ulog
+
+import models.ulog.LogBody
+import play.api.libs.json._
+import play.api.mvc._
+
+trait LogService {
+
+  def saveLog(logbody: JsResult[LogBody]):Result
+
+}
